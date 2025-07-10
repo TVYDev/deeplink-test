bd40b1e12dbd517d547e280e027f84426bf30c46
TANG Vannyou <31821750+TVYDev@users.noreply.github.com>
2025-07-10 11:31:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function handleAppDownload(platform) {
       window.location.href = intentUrl;
     } else if (isIOS) {
       // iOS: Try app scheme
-      window.location.href = `hatthabank://`;
+      window.location.href = `hatthabank://open`;
       
     }
 
